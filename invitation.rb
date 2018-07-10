aa8bf836d8c70d90acf221b88fe63ddc138b16b4
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-10 14:17:25 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,10 +2,10 @@
 # Your friend Harry Potter was very impressed with the invitations that you created for Ron Weasley's graduation party. Harry suggests that the two of you start an invitation printing business together. He wants you to create a program that prompts customers for the following party details...
 
 # * guest_name = gets.chomp
-# * party_name = "the Best Halloween Party Ever"
-# * date = "October 31"
-# * time = "6pm"
-# * host_name = "Harry Potter"
+# * party_name = 
+# * date = 
+# * time = 
+# * host_name = 
 
 # ...and then prints out custom invitations that look something like this:
 
