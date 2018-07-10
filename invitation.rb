e47f236ceca58ab59a32fd410d08264168a357b2
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-10 14:03:19 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,21 @@
 # Code your prompts here!
+Your friend Harry Potter was very impressed with the invitations that you created for Ron Weasley's graduation party. Harry suggests that the two of you start an invitation printing business together. He wants you to create a program that prompts customers for the following party details...
 
+* guest_name = gets.chomp
+* party_name = "the Best Halloween Party Ever"
+* date = "October 31"
+* time = "6pm"
+* host_name = "Harry Potter"
+
+...and then prints out custom invitations that look something like this:
+
+```
+Dear Hermione,
+
+You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+Sincerely,
+
+Harry Potter
+```
 # Try starting out with puts'ing a string.
